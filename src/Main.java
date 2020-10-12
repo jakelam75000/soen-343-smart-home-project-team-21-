6bf5aa68e6f8824d6e35d4c11e8bc0484ebd16f9
Dina Khalesi <dinakhalesit@gmail.com>
2020-10-12 11:23:04 -0400

    Removed main in HouseReader

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import javax.swing.*;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args){
+
         SmartHomeDashboard.setUpLoginFrame();
         // Creates users
         UserManager.initialize();
+
+        // This is to read house layout file submitted by user (right now it is hardcoded to HouseLayout.txt
+        HouseReader.readHouseLayout("Houselayout.txt");
     }
 }
