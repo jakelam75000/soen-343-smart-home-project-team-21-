213ad9daf15839da6ddf3d611125c9eea4ee5ef7
sham-a <ayahshamma123@gmail.com>
2020-10-26 12:12:52 -0400

    Created a Light class

---
+++ src/main/java/Light.java
@@ -0,0 +1,58 @@
+/**
+ * class Light is a smart object and is in every room
+ */
+public class Light extends Smartobj {
+    private boolean ON;
+
+    /**
+     * constructor
+     * @param name
+     */
+    public Light(String name) {
+        super(name, SmartObjectType.LIGHT);
+    }
+
+    /**
+     * Copy constructor
+     * @param light
+     */
+    public Light (Light light) {
+        super(light);
+        this.ON = light.isON();
+    }
+
+    /**
+     * accessor method
+     * @return
+     */
+    public boolean isON() {
+        return ON;
+    }
+
+    /**
+     * mutator method
+     * @param ON
+     */
+    public void setON(boolean ON) {
+        this.ON = ON;
+    }
+
+    /**
+     * returns light name and state
+     * @return
+     */
+    @Override
+    public String toString() {
+        String value = ON? "on":"off";
+        return "This is window " + super.name + " and it is "+ value +"\n";
+    }
+
+    /**
+     * clones the light
+     * @return
+     */
+    @Override
+    public Light clone() {
+        return new Light(this);
+    }
+}
