886618cbd5da29986fdc4899fa9b98606ea4ccb1
Mohamad <42791526+MohamadGhanmeh@users.noreply.github.com>
2020-10-31 15:47:28 -0400

    Added a new Form that will be used for the Alert mechanic of the security system if it finds a thief while the user is set to be in away mode.:)

---
+++ src/main/java/AlertConfirmationWindow.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class AlertConfirmationWindow {
+    private JLabel DecreasingTimer;
+    private JButton cancelAlertButton;
+    private JButton confirmAlertButton;
+}
