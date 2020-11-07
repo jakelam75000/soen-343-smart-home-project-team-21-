de36439d7c6138a32b63076e3dc1877c4a4501d1
jakelam75000 <jlamothe75000@gmail.com>
2020-11-07 18:44:28 -0500

    added back in my junit test

--- src/test/java/SHCCorefunctionality.java
+++ src/test/java/SHCCorefunctionality.java
@@ -11,46 +11,45 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class SHCCorefunctionality {
 
-//    /**
-//     * Testing the use case of SHC functionality
-//     */
-//    @Test
-//    public void SHCfunctionalitytest(){
-//        UserDatabaseManager.loadUsers(false);
-//        SmartHomeDashboard shd = new SmartHomeDashboard("test", UserTypes.PARENT.toString(), "Parent", "Houselayout.txt");
-//        JCheckBox[] j = shd.getOpenClosePanel();
-//        JList<SmartObjectType> d = shd.getListItems();
-//        d.setSelectedIndex(0);
-//        Timer t = new Timer(5000, new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                assertTrue(!shd.getHouse().getObjectState(j[0].getText()));
-//                j[0].setSelected(true);
-//                assertTrue(shd.getHouse().getObjectState(j[0].getText()));
-//                d.setSelectedIndex(2);
-//                Timer t1 = new Timer(5000, new ActionListener() {
-//                    @Override
-//                    public void actionPerformed(ActionEvent e) {
-//                        assertTrue(!shd.getHouse().getObjectState(j[0].getText()));
-//                        j[0].setSelected(true);
-//                        assertTrue(shd.getHouse().getObjectState(j[0].getText()));
-//                        d.setSelectedIndex(1);
-//                        Timer t2 = new Timer(5000, new ActionListener() {
-//                            @Override
-//                            public void actionPerformed(ActionEvent e) {
-//                                assertTrue(!shd.getHouse().getObjectState(j[0].getText()));
-//                                j[0].setSelected(true);
-//                                assertTrue(shd.getHouse().getObjectState(j[0].getText()));
-//                                //PUT CODE HERE
-//                            }
-//                        });
-//                    }
-//                });
-//            }
-//        });
-//
-//
-//    }
+    /**
+     * Testing the use case of SHC functionality
+     */
+    @Test
+    public void SHCfunctionalitytest(){
+        UserDatabaseManager.loadUsers(false);
+        SmartHomeDashboard shd = new SmartHomeDashboard("test", UserTypes.PARENT.toString(), "Parent", "Houselayout.txt");
+        JCheckBox[] j = shd.getOpenClosePanel();
+        JList<SmartObjectType> d = shd.getListItems();
+        d.setSelectedIndex(0);
+        Timer t = new Timer(5000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                assertTrue(!shd.getHouse().getObjectState(j[0].getText()));
+                j[0].setSelected(true);
+                assertTrue(shd.getHouse().getObjectState(j[0].getText()));
+                d.setSelectedIndex(2);
+                Timer t1 = new Timer(5000, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        assertTrue(!shd.getHouse().getObjectState(j[0].getText()));
+                        j[0].setSelected(true);
+                        assertTrue(shd.getHouse().getObjectState(j[0].getText()));
+                        d.setSelectedIndex(1);
+                        Timer t2 = new Timer(5000, new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                assertTrue(!shd.getHouse().getObjectState(j[0].getText()));
+                                j[0].setSelected(true);
+                                assertTrue(shd.getHouse().getObjectState(j[0].getText()));
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+
+    }
 
 
     /**
