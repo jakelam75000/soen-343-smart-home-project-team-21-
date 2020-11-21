f3ee80cfea296ca8f63fa4e9b6811b1a912169d5
Mohamad <42791526+MohamadGhanmeh@users.noreply.github.com>
2020-11-21 12:28:21 -0500

    Created a new form called SetSeasons for the user to set his monthly season for the Winter and Summer. "Please make sure to pay attention to the label I placed on the top of the form. This will be needed when getting the months that the user is setting and making sure that the two seasons don't collide.". Hope you guys like it.

---
+++ src/main/java/SetSeasons.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class SetSeasons {
+    private JComboBox WmonthsCombo1;
+    private JComboBox WmonthsCombo2;
+    private JComboBox SmonthsCombo1;
+    private JComboBox SmonthsCombo2;
+    private JButton ConfimSeasonsButton;
+    private JButton CancelSeasonButton;
+    private JLabel collidingMonthsMessage;
+}
