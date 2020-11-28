9a941bcd3b4b2a003f9a85aecf132823ab289656
Mohamad <42791526+MohamadGhanmeh@users.noreply.github.com>
2020-11-28 16:22:39 -0500

    Fix the bug for windom size and the removed the Summer selection for keeping only winter.

--- src/main/java/SmartHomeDashboard.java
+++ src/main/java/SmartHomeDashboard.java
@@ -150,7 +150,7 @@ public class SmartHomeDashboard extends JFrame implements Observable{
     private static final int x = 200;
     private static final int y = 100;
     private static final int width = 1300;
-    private static final int height = 650;
+    private static final int height = 670;
 
     /**
      * Parameterised constructor.
